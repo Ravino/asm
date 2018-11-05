cf65920879da4a60fb2331e8265bee6166ed2ef9
Ravino <yatakayazvezda@yandex.ru>
2018-11-04 23:35:37 -0500

    Lab5 read string from file

---
+++ lab5.s
@@ -0,0 +1,183 @@
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+.LC0:
+	.string	"./data/test.tex"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1577:
+	.cfi_startproc
+	.cfi_personality 0x9b,DW.ref.__gxx_personality_v0
+	.cfi_lsda 0x1b,.LLSDA1577
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$568, %rsp
+	.cfi_offset 3, -24
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1Ev@PLT
+	leaq	-576(%rbp), %rax
+	movl	$8, %edx
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+.LEHB0:
+	call	_ZNSt14basic_ifstreamIcSt11char_traitsIcEEC1EPKcSt13_Ios_Openmode@PLT
+.LEHE0:
+.L3:
+	leaq	-48(%rbp), %rdx
+	leaq	-576(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+.LEHB1:
+	call	_ZSt7getlineIcSt11char_traitsIcESaIcEERSt13basic_istreamIT_T0_ES7_RNSt7__cxx1112basic_stringIS4_S5_T1_EE@PLT
+	movq	(%rax), %rdx
+	subq	$24, %rdx
+	movq	(%rdx), %rdx
+	addq	%rdx, %rax
+	movq	%rax, %rdi
+	call	_ZNKSt9basic_iosIcSt11char_traitsIcEEcvbEv@PLT
+	testb	%al, %al
+	je	.L2
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsIcSt11char_traitsIcESaIcEERSt13basic_ostreamIT_T0_ES7_RKNSt7__cxx1112basic_stringIS4_S5_T1_EE@PLT
+	movq	%rax, %rdx
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	jmp	.L3
+.L2:
+	leaq	-576(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt14basic_ifstreamIcSt11char_traitsIcEE5closeEv@PLT
+.LEHE1:
+	movl	$0, %ebx
+	leaq	-576(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt14basic_ifstreamIcSt11char_traitsIcEED1Ev@PLT
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+	movl	%ebx, %eax
+	jmp	.L9
+.L8:
+	movq	%rax, %rbx
+	leaq	-576(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt14basic_ifstreamIcSt11char_traitsIcEED1Ev@PLT
+	jmp	.L6
+.L7:
+	movq	%rax, %rbx
+.L6:
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+	movq	%rbx, %rax
+	movq	%rax, %rdi
+.LEHB2:
+	call	_Unwind_Resume@PLT
+.LEHE2:
+.L9:
+	addq	$568, %rsp
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1577:
+	.globl	__gxx_personality_v0
+	.section	.gcc_except_table,"a",@progbits
+.LLSDA1577:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 .LLSDACSE1577-.LLSDACSB1577
+.LLSDACSB1577:
+	.uleb128 .LEHB0-.LFB1577
+	.uleb128 .LEHE0-.LEHB0
+	.uleb128 .L7-.LFB1577
+	.uleb128 0
+	.uleb128 .LEHB1-.LFB1577
+	.uleb128 .LEHE1-.LEHB1
+	.uleb128 .L8-.LFB1577
+	.uleb128 0
+	.uleb128 .LEHB2-.LFB1577
+	.uleb128 .LEHE2-.LEHB2
+	.uleb128 0
+	.uleb128 0
+.LLSDACSE1577:
+	.text
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2079:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L12
+	cmpl	$65535, -8(%rbp)
+	jne	.L12
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L12:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2079:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2080:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2080:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.hidden	DW.ref.__gxx_personality_v0
+	.weak	DW.ref.__gxx_personality_v0
+	.section	.data.DW.ref.__gxx_personality_v0,"awG",@progbits,DW.ref.__gxx_personality_v0,comdat
+	.align 8
+	.type	DW.ref.__gxx_personality_v0, @object
+	.size	DW.ref.__gxx_personality_v0, 8
+DW.ref.__gxx_personality_v0:
+	.quad	__gxx_personality_v0
+	.hidden	__dso_handle
